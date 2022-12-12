e709082d191e9bc4a47e2efc40b5197746480bba
carlos-paezf <cpaezferreira@gmail.com>
2022-12-12 18:10:17 -0500

    Fixed private api-token

--- 04-Issues/react-query-issues/src/app/api/githubApi.ts
+++ 04-Issues/react-query-issues/src/app/api/githubApi.ts
@@ -1,9 +1,8 @@
 import axios from "axios"
 
-
 export const githubApiClient = axios.create( {
     baseURL: 'https://api.github.com/repos/facebook/react',
     headers: {
-        Authorization: 'Bearer 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'
+        Authorization: `Bearer ${ import.meta.env.VITE_APP_API_GITHUB }`
     }
 } )
